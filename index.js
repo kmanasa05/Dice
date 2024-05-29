e48a8285f4e3364133aff7552a8692a7acd213dc
kmanasa05 <141155169+kmanasa05@users.noreply.github.com>
2024-05-29 17:18:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function rollDice(){
+    
     var randomnumber1=Math.floor(Math.random()*6)+1;
-
 var randomdiceimage="dice"+ randomnumber1 + ".png";
-
 var randomimagesource1=randomdiceimage;
-
 var image1=document.querySelectorAll("img")[0];
-
 image1.setAttribute("src", randomimagesource1);
 
+    
 var randomnumber2=Math.floor(Math.random()*6)+1;
 var randomimagesource2="dice"+ randomnumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomimagesource2);
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomimagesource2);
 
 if(randomnumber1>randomnumber2){
     document.getElementById("play").innerHTML="Player 1 Wins!🎊"
