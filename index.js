f20b6162619ce49ea64a5da422c5eec4a8f29cf1
kmanasa05 <141155169+kmanasa05@users.noreply.github.com>
2024-05-29 17:20:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function rollDice(){
     
     var randomnumber1=Math.floor(Math.random()*6)+1;
-var randomdiceimage="dice"+ randomnumber1 + ".png";
-var randomimagesource1=randomdiceimage;
+var randomimagesource1="dice"+ randomnumber1 + ".png";
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomimagesource1);
 
