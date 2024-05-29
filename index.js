b259d67f87a4bbc7159bdc91ac65f8143b07c42c
kmanasa05 <141155169+kmanasa05@users.noreply.github.com>
2024-05-29 17:11:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomimagesource1);
 
 var randomnumber2=Math.floor(Math.random()*6)+1;
-var randomimagesource2="images/dice"+ randomnumber2 + ".png";
+var randomimagesource2="dice"+ randomnumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomimagesource2);
 
 if(randomnumber1>randomnumber2){
